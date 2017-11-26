616517dd505dc6d1fa269b8a3d826cb5e243b336
koichi0702 <maki740819@gmail.com>
2017-11-26 07:19:11 +0000

    Update e.ts

--- e.ts
+++ e.ts
@@ -2,10 +2,4 @@ const magik = magikcraft.io;
 
 function e() {
 	magik.declaro("elytra");
-    function hit(times = 5) {
-     magik.doNTimes(function(){magik.infierno();}, times, 1000);
-     magik.satio();
-     magik.satio();
-     magik.satio();
-     magik.satio();
 }
