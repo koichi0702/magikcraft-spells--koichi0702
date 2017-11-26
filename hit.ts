91ae6d571c464c3254977c6eb1bf4b89de9481fa
koichi0702 <maki740819@gmail.com>
2017-11-26 06:43:15 +0000

    Update hit.ts

--- hit.ts
+++ hit.ts
@@ -2,4 +2,9 @@ const magik = magikcraft.io;
 
 function hit(times = 5) {
      magik.doNTimes(function(){magik.infierno();}, times, 1000);
+     magik.satio();
+     magik.satio();
+     magik.satio();
+     magik.satio();
+magik.satio();
 }
