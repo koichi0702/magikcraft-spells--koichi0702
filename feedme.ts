408ef258d09a20787dc40b1f7aba943f5ea171cc
koichi0702 <maki740819@gmail.com>
2017-12-10 08:33:38 +0000

    Update feedme.ts

---
+++ feedme.ts
@@ -0,0 +1,14 @@
+const magik = magikcraft.io;
+
+function feedme() {
+    magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+magik.satio();
+}
