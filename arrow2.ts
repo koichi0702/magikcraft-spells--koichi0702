df0aa4b213611ef082004d77bed2d301372f15c3
koichi0702 <maki740819@gmail.com>
2017-12-23 07:20:14 +0000

    Update arrow2.ts

---
+++ arrow2.ts
@@ -0,0 +1,12 @@
+const magik = magikcraft.io;
+
+function arrow2() {
+
+    function shoot() {
+        const Arrow = Java.type("org.bukkit.entity.Arrow");
+        const p = magik.getSender();
+        p.launchProjectile(Arrow.class);
+    }
+
+    magik.doNTimes(shoot, 30, 200);
+}    
