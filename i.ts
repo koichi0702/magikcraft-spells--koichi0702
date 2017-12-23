de12c67d250f375b7803d70a539853f9118f51e7
koichi0702 <maki740819@gmail.com>
2017-12-23 06:24:31 +0000

    Update i.ts

--- i.ts
+++ i.ts
@@ -3,10 +3,10 @@ const magik = magikcraft.io;
 function i() {
     const there =	magik.memento.getItem();
     if (there){
-    
+     magik.ianuae(there);
     } else {
         const here = magik.hic();
         magik.memento(here);
     }
 
-}
+    }
