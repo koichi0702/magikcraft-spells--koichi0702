1d00351007afa291d2a9d49079aa6a126f1c42eb
koichi0702 <maki740819@gmail.com>
2018-04-09 04:56:26 +0000

    Update zombie.ts

--- zombie.ts
+++ zombie.ts
@@ -4,9 +4,9 @@ function zombie(){
 
     const sender = magik.getSender();
     const EntityType = magik.type('entity.EntityType');
-    const SKELTON = EntityType['SKELTON'];
+    const zombie = EntityType['ZOMBIE'];
 
     for (var i =0; i < 1000; i++){
-        var arrow = sender.getWorld().spawnEntity(magik.aspecto(),SKELTON);
+        var arrow = sender.getWorld().spawnEntity(magik.aspecto(),zombie);
     }
 }
